01eab312fa77fde6c7eafca5d0ca5c2dcb40eb11
Yanick Landry <yanick.landry@gmail.com>
2014-05-18 11:25:08 -0400

    added default plugin header

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: WP Media Orbit Slider
+ * Plugin URI: https://github.com/yanicklandry/WP-Media-Orbit-Slider
+ * Description: WordPress plugin to use Orbit Image Slider (from Zurb Foundation) with media items.
+ * Version: 0.0.1
+ * Author: Yanick Landry
+ * Author URI: http://www.yanick.cf
+ * License: GPL2
+ */
+
+/*  Copyright YEAR  Yanick Landry  (email : yanick.landry@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
